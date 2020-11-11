8cdd81b2379ce61e420cdcd1e28f25d4d16b8ebb
Perry <olsson.perry@gmail.com>
2020-11-10 19:31:20 -0800

    dev script to start server and client

--- Frontend/app/Screens/LaunchScreen.js
+++ Frontend/app/Screens/LaunchScreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native';
+import { ImageBackground, StyleSheet, Image } from 'react-native';
 
-function LaunchScreen(props) {
+function LaunchScreen() {
 
   return (
 
     <ImageBackground style={styles.background} source={require('../assets/stars1(reversed).jpg')}>
-      <Image style={styles.logo} source={require('../assets/Friday(compact).png')}/>
+      <Image style={styles.logo} source={require('../assets/Friday(compact).png')} />
     </ImageBackground>
   );
 }
@@ -14,17 +14,18 @@ function LaunchScreen(props) {
 
 const styles = StyleSheet.create({
   background:
-        { flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center' },
+  {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   logo:
-        { width:250,
-          height: 100,
-          borderRadius: 3,
-          padding:20
-
-        }
-
+  {
+    width: 250,
+    height: 100,
+    borderRadius: 3,
+    padding: 20
+  }
 });
 
 export default LaunchScreen;
