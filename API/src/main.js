d7cd7c21726b401faba617a5007a9419a01bf4c5
Perry Olsson <olsson.perry@gmail.com>
2020-12-17 17:52:32 -0800

    initial apollo server

--- API/src/main.js
+++ API/src/main.js
@@ -1,5 +1,33 @@
-import { users } from "../testResources";
+import { ApolloServer, gql } from "apollo-server";
 
-for (let user of users) {
-  console.log(user);
-}
+import { users } from "../testResources";
+import logger from "./utils/logger";
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    fullName: String!
+    email: String!
+    username: String!
+  }
+
+  type Query {
+    user(id: ID!): User
+    users: [User]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user: (root, args) => users.filter(user => args.id === user.id)[0],
+    users: () => users,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  logger.info(`Server ready at ${url}`);
+});
---
+++ API/src/utils/logger.js
@@ -0,0 +1,14 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.errors({ stack: true }),
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "user-service" },
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
