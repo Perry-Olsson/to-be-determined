3a535be28594d0308029136dff61a01e790fd0cd
Perry Olsson <olsson.perry@gmail.com>
2021-01-07 14:06:59 -0800

    urql set up and start script for both client and api

--- Client/App.js
+++ Client/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { createClient, Provider } from "urql";
 
 import Main from "./src/Main";
 import GalaxyBackground from "./src/components/GalaxyBackground";
 
 const App = () => {
+  const client = createClient({
+    url: "http://localhost:4000/graphql",
+  });
+
   return (
-    <GalaxyBackground>
-      <Main />
-    </GalaxyBackground>
+    <Provider value={client}>
+      <GalaxyBackground>
+        <Main />
+      </GalaxyBackground>
+    </Provider>
   );
 };
 
 export default App;
-
-
-
