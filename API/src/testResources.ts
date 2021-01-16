9d017881c5d454fe7478a0edfba28d8fa06732f2
Perry Olsson <olsson.perry@gmail.com>
2021-01-15 19:12:49 -0800

    test users

---
+++ API/src/testResources.ts
@@ -0,0 +1,39 @@
+import { RegisterInput } from "./modules/user/register/RegisterInput";
+
+export const users: RegisterInput[] = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "doe.john@gmail.com",
+    username: "johny",
+    password: "password",
+  },
+  {
+    firstName: "Rick",
+    lastName: "Sanchez",
+    email: "sanchez.rick@gmail.com",
+    username: "grandpa_rick",
+    password: "password",
+  },
+  {
+    firstName: "Morty",
+    lastName: "Smith",
+    email: "smith.morty@gmail.com",
+    username: "mortified",
+    password: "password",
+  },
+  {
+    firstName: "James",
+    lastName: "Segura",
+    email: "segura.james@gmail.com",
+    username: "ballhog",
+    password: "password",
+  },
+  {
+    firstName: "TestFirstName",
+    lastName: "TestLastName",
+    email: "testlastname.testfirstname@gmail.com",
+    username: "testUsername",
+    password: "password",
+  },
+];
