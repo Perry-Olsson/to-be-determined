b64ce09397d495d83e11ab2769008903543c0a59
Perry Olsson <olsson.perry@gmail.com>
2021-01-16 08:17:31 -0800

    uncommented updateSchema

--- API/src/devServices/clearUsers.ts
+++ API/src/devServices/clearUsers.ts
@@ -1,11 +1,11 @@
 import { MikroORM } from "@mikro-orm/core";
 import { clearUsers } from "./helpers";
 
-// import updateSchema from "../utils/updateSchema";
+import updateSchema from "../utils/updateSchema";
 
 const main = async () => {
   const orm = await MikroORM.init();
-  // await updateSchema(orm);
+  await updateSchema(orm);
   await clearUsers(orm.em);
   orm.close();
 };
