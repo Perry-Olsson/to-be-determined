493df98f398f80d8c9885780d39c84d9463b5671
Perry Olsson <olsson.perry@gmail.com>
2021-05-29 09:13:57 -0700

    heroku build tweaks

--- API/src/mikro-orm.config.ts
+++ API/src/mikro-orm.config.ts
@@ -3,19 +3,17 @@ import path from "path";
 import config from "./utils/config";
 import { Connection, IDatabaseDriver, Options } from "@mikro-orm/core";
 
+const driverOptions = () => {
+  return __prod__ ? { connection: { ssl: { rejectUnauthorized: false } } } : {};
+};
+
 export default {
   migrations: {
     path: path.resolve(__dirname, "migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
-    disableForeignKeys: true,
+    disableForeignKeys: false,
   },
-  driverOptions: {
-    connection: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  },
+  driverOptions: driverOptions(),
   entities: [path.resolve(__dirname, "./entities/**/*.js")],
   entitiesTs: [path.resolve(__dirname, "./entities/**/*.ts")],
   clientUrl: config.databaseUrl,
